290396e186ec8f66e0c328ae29772be0578048fb
Saicharan2k04 <saicharangondrala2004@gmail.com>
2024-10-23 20:49:02 +0530

    fixed footer issue

--- frontend-main/src/app/app.component.ts
+++ frontend-main/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { JournalformComponent } from './journalform/journalform.component';
@@ -14,7 +14,6 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   imports: [
     RouterOutlet,
     NavbarComponent,
-    FooterComponent,
     SignupComponent,
     LoginComponent,
     JournalformComponent,
